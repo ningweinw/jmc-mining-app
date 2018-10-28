8fb3f017ab2e2f5560bbc36b5f0266659b953c64
Ning Wei <wening@microsoft.com>
2018-10-28 22:30:54 +0800

    Added Redis

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ var express = require('express'),
     engines = require('consolidate'),
     assert = require('assert'),
     ObjectId = require('mongodb').ObjectID,
-    url = 'mongodb://c4tssg2:uVIMgFJOLZ7nFPTCqavJakAJRwVsBvOJINWqFzpIRcY6oEuCAaa5uykPVMt1eLxnSti6cOts44GbDXcX8s4gkg%3D%3D@c4tssg2.documents.azure.com:10255/?ssl=true';
+    url = 'mongodb://c4tssg2:uVIMgFJOLZ7nFPTCqavJakAJRwVsBvOJINWqFzpIRcY6oEuCAaa5uykPVMt1eLxnSti6cOts44GbDXcX8s4gkg%3D%3D@c4tssg2.documents.azure.com:10255/?ssl=true',
+    collectionName = 'records',
+    redis = require("redis"),
+    redisHostName = 'c4tssg2cache.redis.cache.windows.net',
+    redisKey = '9PjAetiA3jfHheZEF6erkKcMBqpw9IEdK9c5vvqS4Xk=';
 
 app.use(express.static(__dirname + "/public"));
 
@@ -24,45 +28,86 @@ function errorHandler(err, req, res, next) {
 
 MongoClient.connect(process.env.MONGODB_URI || url,function(err, db){
     assert.equal(null, err);
-    console.log('Successfully connected to MongoDB.');
+    console.log('Successfully connected to MongoDB');
 
-    var records_collection = db.collection('records');
+    var records_collection = db.collection(collectionName);
+    console.log("DB collection ready for: " + collectionName);
     
+    var cache = redis.createClient(6380, redisHostName, 
+        {auth_pass: redisKey, tls: {servername: redisHostName}});
+    console.log("Cache ready for: " + redisHostName);
+
     app.get('/records', function(req, res, next) {
         console.log("Received get /records request");
-        //var options = {};
-        //options["limit"] = 100;
-        //options["skip"] = 0;
-        //records_collection.find({}, options).toArray(function(err, records){
-        records_collection.find({}).toArray(function(err, records){
+        
+        cache.HGETALL(collectionName, function(err, reply) {
             if(err) throw err;
+            else if(reply) {
+                // cache returns a hashtable object (_id : stringified record object)
+                var records = Object.values(reply);
+                console.log("Cache hit, reply size: " + records.length);
+                // convert record string to object
+                for(var i = 0; i < records.length; i++) {
+                    records[i] = JSON.parse(records[i]);
+                }
+                res.json(records);
+            }
+            else {
+                console.log("Cache miss");
+                //var options = {};
+                //options["limit"] = 20;
+                //options["skip"] = 0;
+                //records_collection.find({}, options).toArray(function(err, records){
+                records_collection.find({}).toArray(function(err, records){
+                    if(err) throw err;
+            
+                    console.log("Number of records from DB: " + records.length);
+                    //console.log(records);
+                    res.json(records);
 
-            console.log("Number of records: " + records.length);
-            // console.log(records);
-            res.json(records);
+                    // write to cache as hashtable, key is _id, value is the record object
+                    var i, hashList = [];
+                    for(i = 0; i < records.length; i++) {
+                        hashList[2 * i] = records[i]._id.toString();
+                        hashList[2 * i + 1] = JSON.stringify(records[i]);
+                    }
+                    cache.HMSET(collectionName, hashList);
+                    console.log("Cache set successfully");
+                });
+            }
         });
     });
 
     app.post('/records', function(req, res, next){
-        console.log(req.body);
+        console.log("Received create request for: " + JSON.stringify(req.body));
         records_collection.insert(req.body, function(err, doc) {
             if(err) throw err;
             console.log(doc);
             res.json(doc);
+
+            // update cache
+            var insertedId = doc.insertedIds[0];
+            var insertedRecord = doc.ops[0];
+            cache.HSET(collectionName, insertedId.toString(), JSON.stringify(insertedRecord));
         });
     });
 
     app.delete('/records/:id', function(req, res, next){
         var id = req.params.id;
-        console.log("delete " + id);
+        console.log("Received delete request for: " + id);
         records_collection.deleteOne({'_id': new ObjectId(id)}, function(err, results){
-            console.log(results);
+            if(err) throw err;
+            //console.log(results);
             res.json(results);
+
+            // update cache
+            cache.HDEL(collectionName, id.toString());
         });
     });
 
     app.put('/records/:id', function(req, res, next){
         var id = req.params.id;
+        console.log("Received update request for: " + id);
         records_collection.updateOne(
             {'_id': new ObjectId(id)},
             { $set: {
@@ -71,8 +116,16 @@ MongoClient.connect(process.env.MONGODB_URI || url,function(err, db){
                 'phone': req.body.phone
                 }
             }, function(err, results){
-                console.log(results);
+                if(err) throw err;
+                //console.log(results);
                 res.json(results);
+
+                // update cache
+                var updatedRecord = {'_id' : id,
+                    'name' : req.body.name,
+                    'email': req.body.email,
+                    'phone': req.body.phone};
+                cache.HSET(collectionName, id.toString(), JSON.stringify(updatedRecord));
         });
     });
 
